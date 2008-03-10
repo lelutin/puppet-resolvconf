248c5af0e05478fb7f3b38f8c4b438a88cdc105b
am <am@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-03-10 13:21:57 +0000

    umwandlung in class, und def. von variablen
    git-svn-id: https://svn/ipuppet/trunk/modules/resolvconf@1057 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,25 +4,37 @@
 
 # modules_dir { "resolvconf": }
 
-class resolvconf {}
-
-define resolvconf::create (
-    $domain='glei.ch', 
-    $search='glei.ch',
-    $nameserver1='212.103.67.60',  
-    $nameserver2='212.103.72.231', 
-    $nameserver3='192.168.1.104'
-) {
-    $str = "search $search
-domain $domain
-nameserver $nameserver1
-nameserver $nameserver2
-nameserver $nameserver3
-"
-
-    file { "/etc/resolv.conf":
-        content => $str
+class resolvconf {
+    $real_resolvconf_domain = $resolvconf_domain ? {
+        ''  => 'glei.ch',
+        default => $resolvconf_domain,
     }
+    $real_resolvconf_search = $resolvconf_search ? {
+        ''  => 'glei.ch',
+        default => $resolvconf_search,
+    }
+    $real_resolvconf_nameserver1 = $resolvconf_nameserver1 ? {
+        ''  => '212.103.67.60',
+        default => $resolvconf_nameserver1,
+    }
+    $real_resolvconf_nameserver2 = $resolvconf_nameserver2 ? {
+        ''  => '212.103.72.231',
+        default => $resolvconf_nameserver2,
+    }
+    $real_resolvconf_nameserver3 = $resolvconf_nameserver3 ? {
+        ''  => '192.168.1.104',
+        default => $resolvconf_nameserver3,
+    }
+
+    file { '/etc/resolv.conf':
+        path => '/etc/resolv.conf',
+        owner => root,
+        group => 0,
+        mode => 444,
+        content => template("resolvconf/resolvconf.erb")
+    }
+
+
 }
 
 
