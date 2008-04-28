433c0fa776449d42894ae5937a35f2bb6b53217a
andreas <andreas@immerda.ch>
2008-04-28 10:19:54 +0000

    nameservers now as string with ":" as delimiter

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,18 +13,23 @@ class resolvconf {
         ''  => 'glei.ch',
         default => $resolvconf_search,
     }
-    $real_resolvconf_nameserver1 = $resolvconf_nameserver1 ? {
-        ''  => '212.103.67.60',
+    $real_resolvconf_nameservers ? $resolvconf_nameservers ? {
+        ''  => '212.103.67.60:212.103.67.61:212.103.72.231:192.168.1.104',
         default => $resolvconf_nameserver1,
     }
-    $real_resolvconf_nameserver2 = $resolvconf_nameserver2 ? {
-        ''  => '212.103.72.231',
-        default => $resolvconf_nameserver2,
-    }
-    $real_resolvconf_nameserver3 = $resolvconf_nameserver3 ? {
-        ''  => '192.168.1.104',
-        default => $resolvconf_nameserver3,
-    }
+
+#    $real_resolvconf_nameserver1 = $resolvconf_nameserver1 ? {
+#        ''  => '212.103.67.60',
+#        default => $resolvconf_nameserver1,
+#    }
+#    $real_resolvconf_nameserver2 = $resolvconf_nameserver2 ? {
+#        ''  => '212.103.72.231',
+#        default => $resolvconf_nameserver2,
+#    }
+#    $real_resolvconf_nameserver3 = $resolvconf_nameserver3 ? {
+#        ''  => '192.168.1.104',
+#        default => $resolvconf_nameserver3,
+#    }
 
     file { '/etc/resolv.conf':
         path => '/etc/resolv.conf',
