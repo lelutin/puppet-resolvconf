5e4ef43b56f76a57b93be016d8efa6418cd57760
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 11:12:38 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# resolvconf module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "resolvconf": }
+class resolvconf {}
