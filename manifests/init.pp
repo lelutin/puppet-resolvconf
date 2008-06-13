018232e2117764e636e6fc53ccd65cd4a60c9409
mh <mh@immerda.ch>
2008-06-13 20:26:23 +0000

    merged with puzzle

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,16 @@
-# modules/resolvconf/manifests/init.pp - manage resolvconf stuff
-# Copyright (C) 2007 admin@immerda.ch
-# GPLv3
+#
+# resolvconf module
+#
+# Copyright 2008, admin(at)immerda.ch
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "resolvconf": }
 
@@ -25,9 +35,4 @@ class resolvconf {
         mode => 444,
         content => template("resolvconf/resolvconf.erb")
     }
-
-
 }
-
-
-
