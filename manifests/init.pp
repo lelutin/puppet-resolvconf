991c3aa7aa2dfa10f3b34a1158774abf7614461b
mh <mh@immerda.ch>
2014-08-15 17:34:17 +0200

    improve handling if we don't have a domain

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,22 +6,28 @@
 # Marcel Haerry haerry+puppet(at)puzzle.ch
 # Simon Josi josi+puppet(at)puzzle.ch
 #
-# This program is free software; you can redistribute 
-# it and/or modify it under the terms of the GNU 
-# General Public License version 3 as published by 
+# This program is free software; you can redistribute
+# it and/or modify it under the terms of the GNU
+# General Public License version 3 as published by
 # the Free Software Foundation.
 #
 
 class resolvconf(
-  $domain = $::domain,  
+  $domain = $::domain,
   $search = $::domain,
-  $nameservers = [ '8.8.8.8' ]
+  $nameservers = [
+    '77.109.138.45',
+    '77.109.139.29',
+  ]
 ) {
+  $content = $::operatingsystem ? {
+    openbsd => template("resolvconf/resolvconf.${::operatingsystem}.erb"),
+    default => template('resolvconf/resolvconf.erb'),
+  }
   file{'/etc/resolv.conf':
-    content => $::operatingsystem ? {
-      openbsd => template("resolvconf/resolvconf.${::operatingsystem}.erb"),
-      default => template('resolvconf/resolvconf.erb'),
-    },
-    owner => root, group => 0, mode => 0444;
+    content => $content,
+    owner   => root,
+    group   => 0,
+    mode    => '0444';
   }
 }
